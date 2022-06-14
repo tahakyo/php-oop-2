78bae1e8cd4872d6ea621eb1f7b27efd4baefcf5
tahakyo <ghazouani1988@email.com>
2022-06-14 22:12:02 +0200

    add list of products

--- index.php
+++ index.php
@@ -10,6 +10,21 @@ error_reporting(E_ALL);
 <?php
     require_once __DIR__. "/product.php";
     require_once __DIR__. "/food.php";
+    require_once __DIR__. "/accessory.php";
+    require_once __DIR__. "/toy.php";
+    require_once __DIR__. "/user.php";
+
+    // products
+
+    $catFood = new food("Royal canin", "Umido per gatti gusto tonno e formaggio", 1.19, "85gr");
+    $dogFood = new food("Natural Trainer", "Croccantini per cane gusto anatra e patate", 9.99, "750gr");
+    $catToy = new toy("cinesata", "Gioco d'intelligenza con due gradi di difficoltà", 2.50, "Rosso");
+    $dogAccessory = new accessory("Elastofit", "Gilet rinfrescante", 30 , "PVA");
+
+    // Users
+
+    
+
     
 ?>
 <!DOCTYPE html>
---
+++ user.php
@@ -0,0 +1,18 @@
+<?php
+    class user {
+      public $name;
+      public $lastName;
+      public $mail;
+      public $cart = [];
+      public $cc_expiry = false;
+      public $membership = false;
+      private $discount;
+
+      function __construct($_name, $_lastName, $_mail)
+      {
+        $this->name = $_name;
+        $this->lastName = $_lastName;
+        $this->mail = $_mail;
+      }
+    }
+?>
